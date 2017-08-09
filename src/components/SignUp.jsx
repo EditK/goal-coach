f3f4b7c512252f2339107e4d1442f82f0d15b580
Kapcari-Kuhn Edite <kapedi@orbis.de>
2017-08-09 14:19:04 +0200

    Errors integrated in SignUp

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,42 +1,55 @@
 import React, { Component } from 'react';
+import { firebaseApp } from '../firebase';
 
 class SignUp extends Component {
     constructor(props){
         super(props);
         this.state = {
             email: '',
-            password: ''
+            password: '',
+            error: {
+                message: ''
+            }
         }
     }
 signUp() {
     console.log('this.state', this.state);
+    const { email, password } = this.state;
+    firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+        .catch(error => {
+            this.setState({error})
+        })
 }
 
     render() {
         return (
-            <div className="form-inLine">
-                <h2>Signup</h2>
+            <div className="form-inLine" style={{margin: '5%', width: '20%'}}>
+                <h2>Sign Up</h2>
                 <div className="form-group">
                     <input
                         className="form-control"
                         type="text"
+                        style={{marginRight: '5px', marginTop: '1%'}}
                         placeholder="email"
                         onChange={event => this.setState({email: event.target.value})}
                     />
                     <input
                         className="form-control"
                         type="password"
+                        style={{marginRight: '5px', marginTop: '1%'}}
                         placeholder="password"
                         onChange={event => this.setState({password: event.target.value})}
                     />
                     <button
                     className="btn btn-primary"
                     type="button"
+                    style={{marginTop: '1%'}}
                     onClick={() => this.signUp()}
                     >
                     Sign Up
                     </button>
                 </div>
+                <div>{this.state.error.message}</div>
             </div>
         )
     }
